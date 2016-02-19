2e8c469995bf5995d6c2e99d7bad20b378d2f9e5
Sam Crognale <sam.crognale@gmail.com>
2016-02-19 14:40:11 -0800

    images upload to server. Not associated with user nor displayed anywhere

---
+++ client/app.js
@@ -0,0 +1,10 @@
+Template.imageUpload.events({
+	'change .fileInput': function(event, template) {
+		console.log("fileInput changed")
+		var files = event.target.files;
+		Images.insert(files[0], function (err, fileObj) {
+			//Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+			console.log("inserted now file with id: ", fileObj._id);
+		});
+	}
+});
---
+++ server/server.js
@@ -0,0 +1,11 @@
+Images.allow({
+	'update': function() {
+		//add custom authentication code here
+		return true;
+	},
+	'insert': function() {
+		//add custom authentication code here
+		return true;
+	}
+});
+
